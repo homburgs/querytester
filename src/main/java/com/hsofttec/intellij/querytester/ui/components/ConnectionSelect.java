8b5345ac4b60d55da65dfbde5e64579203fa83c3
S.Homburg <S.Homburg@ceyoniq.com>
2022-09-23 01:14:52 +0200

    massive changes after early first release

--- src/main/java/com/hsofttec/intellij/querytester/ui/components/ConnectionSelect.java
+++ src/main/java/com/hsofttec/intellij/querytester/ui/components/ConnectionSelect.java
@@ -1,35 +1,88 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2022 Sven Homburg, <homburgs@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.hsofttec.intellij.querytester.ui.components;
 
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.hsofttec.intellij.querytester.events.ConnectionAddedEvent;
+import com.hsofttec.intellij.querytester.events.ConnectionRemovedEvent;
 import com.hsofttec.intellij.querytester.events.ConnectionSelectionChangedEvent;
-import com.hsofttec.intellij.querytester.models.ConnectionConfigurationComboBoxModel;
 import com.hsofttec.intellij.querytester.renderer.ConnectionListCellRenderer;
+import com.hsofttec.intellij.querytester.services.ConnectionService;
 import com.hsofttec.intellij.querytester.services.ConnectionSettingsService;
 import com.hsofttec.intellij.querytester.ui.EventBusFactory;
+import com.intellij.ui.CollectionComboBoxModel;
 
 import javax.swing.*;
 import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class ConnectionSelect extends JComboBox<ConnectionSettingsService.ConnectionSettings> {
+public class ConnectionSelect extends JComboBox<ConnectionSettingsService.ConnectionSettings> implements ItemListener {
     private static final EventBus EVENT_BUS = EventBusFactory.getInstance( ).get( );
     private static final ConnectionSettingsService connectionSettingsService = ConnectionSettingsService.getSettings( );
 
+    private static final ConnectionService CONNECTION_SERVICE = ConnectionService.getInstance( );
+
     public ConnectionSelect( ) {
         EVENT_BUS.register( this );
-        this.addItemListener( itemEvent -> {
-            if ( itemEvent.getStateChange( ) == ItemEvent.SELECTED ) {
-                EVENT_BUS.post( new ConnectionSelectionChangedEvent( ( ConnectionSettingsService.ConnectionSettings ) itemEvent.getItem( ) ) );
-            }
-        } );
-
+        this.addItemListener( this );
         setRenderer( new ConnectionListCellRenderer( ) );
-        if ( !connectionSettingsService.connectionSettingsState.connectionSettings.isEmpty( ) ) {
-            setModel( new ConnectionConfigurationComboBoxModel( connectionSettingsService.connectionSettingsState.connectionSettings.toArray( new ConnectionSettingsService.ConnectionSettings[]{ } ) ) );
-            setSelectedIndex( 0 );
-            EVENT_BUS.post( new ConnectionSelectionChangedEvent( ( ConnectionSettingsService.ConnectionSettings ) getSelectedItem( ) ) );
+        setModel( new CollectionComboBoxModel<>( connectionSettingsService.connectionSettingsState.connectionSettings ) );
+        ConnectionSettingsService.ConnectionSettings settings = ( ConnectionSettingsService.ConnectionSettings ) getSelectedItem( );
+
+        if ( isConnectionUsable( settings ) ) {
+            EVENT_BUS.post( new ConnectionSelectionChangedEvent( settings ) );
+        } else {
+            EVENT_BUS.post( new ConnectionSelectionChangedEvent( null ) );
         }
+    }
+
+    @Override
+    public void itemStateChanged( ItemEvent itemEvent ) {
+        if ( itemEvent.getStateChange( ) == ItemEvent.SELECTED ) {
+            ConnectionSettingsService.ConnectionSettings settings = ( ConnectionSettingsService.ConnectionSettings ) getSelectedItem( );
+            if ( isConnectionUsable( settings ) ) {
+                EVENT_BUS.post( new ConnectionSelectionChangedEvent( settings ) );
+            } else {
+                EVENT_BUS.post( new ConnectionSelectionChangedEvent( null ) );
+            }
+        }
+    }
+
+    private boolean isConnectionUsable( ConnectionSettingsService.ConnectionSettings settings ) {
+        boolean usable = false;
+
+        if ( settings != null ) {
+            try {
+                CONNECTION_SERVICE.createConnection( settings );
+                usable = true;
+            } catch ( Exception e ) {
+            }
+        }
+
+        return usable;
     }
 
     @Subscribe
@@ -39,5 +92,10 @@ public class ConnectionSelect extends JComboBox<ConnectionSettingsService.Connec
             setSelectedIndex( 0 );
             EVENT_BUS.post( new ConnectionSelectionChangedEvent( ( ConnectionSettingsService.ConnectionSettings ) getSelectedItem( ) ) );
         }
+    }
+
+    @Subscribe
+    public void connectionSettingsRemoved( ConnectionRemovedEvent event ) {
+        removeItem( event.getData( ) );
     }
 }
--- src/main/java/com/hsofttec/intellij/querytester/ui/components/MasterdataScopeSelect.java
+++ src/main/java/com/hsofttec/intellij/querytester/ui/components/MasterdataScopeSelect.java
@@ -1,5 +1,30 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright © 2022 Sven Homburg, <homburgs@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.hsofttec.intellij.querytester.ui.components;
 
+import com.ceyoniq.nscale.al.core.Session;
 import com.ceyoniq.nscale.al.core.cfg.MasterdataScope;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
@@ -11,44 +36,47 @@ import com.intellij.ui.CollectionComboBoxModel;
 
 import javax.swing.*;
 import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class MasterdataScopeSelect extends JComboBox<String> {
+public class MasterdataScopeSelect extends JComboBox<String> implements ItemListener {
     private static final EventBus EVENT_BUS = EventBusFactory.getInstance( ).get( );
     private static final ConnectionService connectionService = ConnectionService.getInstance( );
     private final List<String> scopes = new ArrayList<>( );
 
     public MasterdataScopeSelect( ) {
         EVENT_BUS.register( this );
-        this.addItemListener( itemEvent -> {
-            if ( itemEvent.getStateChange( ) == ItemEvent.SELECTED ) {
-                EVENT_BUS.post( new DocumentAreaChangedEvent( ( String ) itemEvent.getItem( ) ) );
-            }
-        } );
-
-//        setRenderer( new ConnectionListCellRenderer( ) );
         setModel( new CollectionComboBoxModel<>( scopes ) );
-        setSelectedIndex( 0 );
+        addItemListener( this );
     }
 
     @Override
     public void setSelectedIndex( int index ) {
-        if ( !scopes.isEmpty( ) && scopes.size( ) >= index ) {
-            super.setSelectedIndex( index );
-            EVENT_BUS.post( new MasterdataScopeChangedEvent( ( String ) getSelectedItem( ) ) );
-        }
+        super.setSelectedIndex( index );
+        EVENT_BUS.post( new MasterdataScopeChangedEvent( ( String ) getSelectedItem( ) ) );
     }
 
     @Subscribe
     public void documentAreaChangedEvent( DocumentAreaChangedEvent event ) {
-        List<MasterdataScope> masterdataScopes = connectionService.getSession( ).getConfigurationService( ).getMasterdataScopes( event.getDocumentAreaName( ) );
+        Session session = connectionService.getSession( );
         scopes.clear( );
-        for ( MasterdataScope masterdataScope : masterdataScopes ) {
-            scopes.add( masterdataScope.getDisplayNameId( ) );
+        setSelectedIndex( -1 );
+        if ( session != null ) {
+            List<MasterdataScope> masterdataScopes = session.getConfigurationService( ).getMasterdataScopes( event.getDocumentAreaName( ) );
+            scopes.addAll( masterdataScopes.stream( ).map( MasterdataScope::getDisplayNameId ).collect( Collectors.toList( ) ) );
+            if ( !scopes.isEmpty( ) ) {
+                setSelectedIndex( 0 );
+            }
         }
+    }
 
-        setSelectedIndex( 0 );
+    @Override
+    public void itemStateChanged( ItemEvent itemEvent ) {
+        if ( itemEvent.getStateChange( ) == ItemEvent.SELECTED ) {
+            EVENT_BUS.post( new MasterdataScopeChangedEvent( ( String ) itemEvent.getItem( ) ) );
+        }
     }
 }
 
